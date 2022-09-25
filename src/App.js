f5287cac467741d677d899d43e46ce14fe226ae8
Mulualem Eshetu <mulualem@ienetworks.co>
2022-09-25 14:41:44 +0300

    state and setState in class components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: "Mulualem", lastName: "Eshetu" },
+      company: "IE Networks",
+    };
+  }
+
+  render() {
+    const handleClick = (e) => {
+      this.setState(
+        () => {
+          return {
+            name: { firstName: "Yosef", lastName: "Worku" },
+            company: "Addis Software",
+          };
+        },
+        () => {
+          console.log(this.state);
+        }
+      );
+    };
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hii {this?.state?.name?.firstName} {this?.state?.name?.lastName}, I
+            work at {this?.state?.company}
+          </p>
+          <button onClick={(e) => handleClick(e)}>Change Name</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
